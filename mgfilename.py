6fa4f76c1e0aff6a4101a9cbeab9449459bb7a4d
Liza Kolesnikova <ekolesnikova0031@gmail.com>
2019-05-27 21:00:23 +0300

    Footer content update
    Added email submission form, updated text of automated email, updated SM links

--- mgfilename.py
+++ mgfilename.py
@@ -10,7 +10,7 @@ def send_simple_message(email):
       "https://api.mailgun.net/v3/sandbox2e97bfad21db449b811b676424e904c2.mailgun.org/messages",
        #auth=("api", "3525b933755ecceab47c9013a469ab4c-e566273b-25ecab3e"),
        auth=("api", api_key),
-       data={"from": "Excited User <aunahine@sun.ru>",
+       data={"from": "Your travel advisor <sunshine@sun.com>",
              "to": [email],
              "subject": "Your perfect holiday!",
-             "text": "Thank "})
+             "text": "Thank you for subscribing for our newsletter. We will keep you up-to-date with all hot deals, trendy travel destinations and inspiring ideas for your vacation."})
